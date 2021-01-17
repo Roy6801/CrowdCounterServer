fce9fcda974246f72bfdd9d018523ce90117bd5a
Roy6801 <mondal6801@gmail.com>
2021-01-17 21:33:19 +0530

    structure changed

---
+++ CCS/myUtils/pyrebaseConnector/Connector.py
@@ -0,0 +1,32 @@
+import pyrebase
+import threading
+import json
+import os
+
+pyrebaseFile = open(os.getcwd()+"/cc-6/pyrebase.json","r")
+firebaseConfig = json.load(pyrebaseFile)
+pyrebaseFile.close()
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
+
+def set_app(host,name):
+    global db
+    db.child("Servers").child(host["name"]).child("app").set(name)
+
+
+class Connection:
+    def __init__(self, cid, host):
+        global db
+        self.count = ""
+        self.cid = cid
+        self.host = host
+        db.child("Servers").child(self.host["name"]).set(self.host)
+
+    def set_count(self, count):
+        self.count = count
+
+    def update_count(self):
+        global db
+        db.child("Servers").child(self.host["name"]).child("Cameras").child(self.cid).set(str(self.count))
+        threading.Timer(0.3, self.update_count).start()
---
+++ Launcher.py
@@ -0,0 +1,7 @@
+import os
+
+print("\n\n********** Installing Requirements *********")
+os.system('pip3 install -r requirements.txt')
+os.chdir(os.getcwd()+'/CCS')
+print("\n\n********** Initializing *********")
+os.system('python Host.py')
