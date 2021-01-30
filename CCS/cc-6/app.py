b655203b8b7e4679b8d62625123ad3f917e2fc5e
Roy6801 <>
2021-01-30 21:23:10 +0530

    js

--- CCS/cc-6/app.py
+++ CCS/cc-6/app.py
@@ -10,12 +10,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Hi"
 
 @app.route("/count")
 def count():
     global conn
-    return "Hi"
+    return render_template("rough.html",dict=camDict)
 
 if __name__ == "__main__":
 	app.run(port=environ.get('PORT'),threaded=True)
