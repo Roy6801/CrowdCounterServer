b7f97c31e3e93ba8abafaaecbbb8885ca01dc7bd
Lina-Pawar <lpawar2911@gmail.com>
2021-02-01 11:03:54 +0530

    commit

--- CCS/cc-6/app.py
+++ CCS/cc-6/app.py
@@ -10,12 +10,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "hi"
 
 @app.route("/count")
 def count():
     global conn
-    return "Hi"
+    return render_template("index.html",co=conn.camDict)
 
 if __name__ == "__main__":
 	app.run(port=environ.get('PORT'),threaded=True)
