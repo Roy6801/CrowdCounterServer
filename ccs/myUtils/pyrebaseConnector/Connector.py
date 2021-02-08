1f2c166cebe45933f789b1bf31009df4d9f8cc4c
Roy6801 <>
2021-02-08 22:20:30 +0530

    Plotter Update

--- ccs/myUtils/pyrebaseConnector/Connector.py
+++ ccs/myUtils/pyrebaseConnector/Connector.py
@@ -36,26 +36,3 @@ class Connection:
         global db
         db.child("Servers").child(self.host["name"]).child("Cameras").child(self.cid).set(str(self.count))
         threading.Thread(target=self.update_count).start()
-        timestamp = int(time.time())
-        if timestamp % 10 == 0:
-            threading.Thread(target=self.record,args=(timestamp,)).start()
-
-    def record(self, ts):
-        global db, counter
-        total = db.child("Hosts").child(self.host["name"]).get()
-        if total.val() is not None:
-            self.plots[int(ts)] = int(total.val())
-            counter = counter + 1
-        if counter == 6:
-            with open(self.host["name"]+".json","w") as file:
-                json.dump(self.plots,file)
-            counter = 0
-        self.erase(ts)
-
-    def erase(self, ts):
-        temp = list()
-        for i in self.plots:
-            if i < (ts - 50):
-                temp.append(i)
-        for j in temp:
-            del self.plots[j]
